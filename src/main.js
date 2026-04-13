70a2448d7fe1fd886ec97506ab08349954cc4d7d
Noctus Rex <noxjudex@gmail.com>
2026-04-13 20:37:19 +0200

    test

--- src/main.js
+++ src/main.js
@@ -13,9 +13,10 @@ export default async ({ req, res, log, error }) => {
   }
 
   const client = new Client()
-    .setEndpoint("http://172.17.0.1/v1")
+    .setEndpoint("http://appwrite.reallygreat.games/v1")
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(process.env.APPWRITE_API_KEY);
+    .setKey(process.env.APPWRITE_API_KEY)
+    .setSelfSigned(true);
 
   const databases = new Databases(client);
   const createdIds = [];
